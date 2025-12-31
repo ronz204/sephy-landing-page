6ff71a5fdd0508cef706e14a4b58d109179d5dec
roney ruiz <roneyruizrojas@gmail.com>
2025-12-31 15:59:11 -0600

    feat: create a example of react routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,14 @@
 import "./index.css";
 import { App } from "./App";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
 
 const root = document.getElementById("root")!;
-createRoot(root).render(<App />);
+
+createRoot(root).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
+  </BrowserRouter>
+);
