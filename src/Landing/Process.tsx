17fe499635a0e0a6e4c4ff37035f0b154b8a09d7
roney ruiz <roneyruizrojas@gmail.com>
2026-01-04 18:20:53 -0600

    style: adjust process vertical padding

--- src/Landing/Process.tsx
+++ src/Landing/Process.tsx
@@ -3,7 +3,7 @@ import { Steps } from "@Constants/Steps";
 
 export const Process = (): JSX.Element => {
   return (
-    <section id="process" className="bg-gray-50 py-24 sm:py-32">
+    <section id="process" className="bg-gray-50 py-24">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
 
         <div className="mx-auto max-w-2xl text-center">
